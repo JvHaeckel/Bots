d72c4f934080aeae91ca56c23e8f0656f3fc27b0
JvHaeckel <joaohaeckel@gmail.com>
2024-07-04 10:45:28 -0300

    SEBRAE

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const puppeteer = require("puppeteer"); // Constante para importar o pupeteer
+/* CRIE SEU PRIMEIRO BOT QUE ENVIA MENSAGENS AUTOMATICAMENTE NO WHATS APP!
 
+https://www.youtube.com/watch?v=CrH3AqWF6g0&t=1s&ab_channel=DankiCode*/
+
+
+const puppeteer = require("puppeteer"); // Constante para importar o pupeteer
 
 (async function main() {  // Função do bot
     try {
@@ -16,9 +20,23 @@ const puppeteer = require("puppeteer"); // Constante para importar o pupeteer
         await page.goto("https://web.whatsapp.com/")
 
         /* Fazer o bot procurar nossos contatos*/
-        await page.waitForSelector("") // Essa classe muda com o tempo
+        await page.waitForSelector("._2_1wd") // Essa classe muda com o tempo
+        await delay(5000); // Para o bot esperar 5000 milisegundos
+
+        const contactName = ""; // Nome do contato em vazio
+        await page.click(`span[title = ${contactName}]`)
+
+        // Parei aqui aos 11:59 
+        await page.waitForSelector("OTBsx");
+
 
     } catch (e) {
         console.error("Lascou deu erro", e);
     }
 })();
+
+function delay(time){
+    return new Promise(function (resolve){
+        setTimeout(resolve,time);
+    });
+}
